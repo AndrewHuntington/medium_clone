5096d69d2a034987e5f2fbcaad88662896505f18
Andrew Huntington <andrew.huntington@gmail.com>
2022-08-16 19:39:39 +0900

    Update post/[slug] to handle comments

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -3,6 +3,7 @@ import Header from "../../components/Header";
 import { Post } from "../../typings";
 import { sanityClient, urlFor } from "../../sanity";
 import PortableText from "react-portable-text";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 // tells Next.js how to figure out which paths to pre-render
 export const getStaticPaths = async () => {
@@ -63,11 +64,37 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   };
 };
 
+interface IFormInput {
+  _id: string;
+  name: string;
+  email: string;
+  comment: string;
+}
+
 interface Props {
   post: Post;
 }
 
 const Post: NextPage<Props> = ({ post }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit: SubmitHandler<IFormInput> = async (data) => {
+    await fetch("/api/createComment", {
+      method: "POST",
+      body: JSON.stringify(data),
+    })
+      .then(() => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
   return (
     <main>
       <Header />
@@ -124,6 +151,68 @@ const Post: NextPage<Props> = ({ post }: Props) => {
           />
         </div>
       </article>
+
+      <hr className="max-w-lg mx-auto my-5 border border-yellow-500" />
+
+      {/* Form Area */}
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col max-w-2xl p-5 mx-auto mb-10"
+      >
+        <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+        <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+        <hr className="py-3 mt-2" />
+
+        <input {...register("_id")} type="hidden" name="_id" value={post._id} />
+
+        <label className="block mb-5">
+          <span className="text-gray-700">Name</span>
+          <input
+            {...register("name", { required: true })}
+            className="block w-full px-3 py-2 mt-1 border rounded shadow outline-none form-input ring-yellow-500 focus:ring"
+            placeholder="Johnny B. Goode"
+            type="text"
+          />
+        </label>
+        <label className="block mb-5">
+          <span className="text-gray-700">Email</span>
+          <input
+            {...register("email", { required: true })}
+            className="block w-full px-3 py-2 mt-1 border rounded shadow outline-none form-input ring-yellow-500 focus:ring"
+            placeholder="johnny@go.com"
+            type="email"
+          />
+        </label>
+        <label className="block mb-5">
+          <span className="text-gray-700">Comment</span>
+          <textarea
+            {...register("comment", { required: true })}
+            className="block w-full px-3 py-2 mt-1 border rounded shadow outline-none form-text-area ring-yellow-500 focus:ring"
+            placeholder="Please leave your comment here"
+            rows={8}
+          />
+        </label>
+
+        {/* errors return when field validation fails */}
+        <div className="flex flex-col p-5">
+          {errors.name && (
+            <span className="text-red-500">- The Name Field is required</span>
+          )}
+          {errors.email && (
+            <span className="text-red-500">- The Email Field is required</span>
+          )}
+          {errors.comment && (
+            <span className="text-red-500">
+              - The Comment Field is required
+            </span>
+          )}
+        </div>
+
+        <input
+          type="submit"
+          className="px-4 py-2 font-bold text-white bg-yellow-500 rounded shadow cursor-pointer hover:bg-yellow-400 focus:shadow-outline focus:outline-none"
+        />
+      </form>
     </main>
   );
 };
